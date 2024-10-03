79e2136f340a6e89a97309335d75971c0b76ba2c
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2024-10-03 17:29:36 +0000

    added rgb

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,30 @@
 """
 Created by: Mr. Coxall
 Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+This module is a Micro:bit MicroPython program runs RGB lights 
 """
 
 from microbit import *
 
+# setting the microbit
+display.clear()
+# setting the colors
+if button_a.get_presses():
+    pin13.write_digital(0)
+    pin14.write_digital(0)
+    pin15.write_digital(0)
 
-display.scroll("Hello, World!")
+    # red ligth
+    pin13.write_analog(1)
+    pin14.write_digital(0)
+    pin15.write_digital(0)
+
+    # blue ligth
+    pin13.write_analog(0)
+    pin14.write_digital(1)
+    pin15.write_digital(0)
+
+    # green light
+    pin13.write_analog(0)
+    pin14.write_digital(0)
+    pin15.write_digital(1)
